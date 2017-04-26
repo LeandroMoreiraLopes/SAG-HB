744ad1c9f70db19055fb07a145cdc2e8484f0cd4
Leandro Lopes <lecomlopes@gmail.com>
2017-04-25 23:57:43 -0300

    formata data funcionando. OBS: mudar data no banco para int

--- Assets/Scripts/Utils/FormatarData.cs
+++ Assets/Scripts/Utils/FormatarData.cs
@@ -8,8 +8,8 @@ public class FormatarData : MonoBehaviour {
     {
         string s = i.ToString();
         string ano = s.Substring(0, 4);
-        string mes = s.Substring(5, 2);
-        string dia = s.Substring(7, 2);
+        string mes = s.Substring(4, 2);
+        string dia = s.Substring(6, 2);
         s = dia + "/" + mes + "/" + ano;
         return s;
     }
