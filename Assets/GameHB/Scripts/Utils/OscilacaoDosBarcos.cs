bd8f9af388ecb7ef9f8f115650d12159c671f016
Leandro Lopes <lecomlopes@gmail.com>
2017-06-04 17:36:46 -0300

    adicionados modelos das batalhas da invasao holandesa, do descobrimento do brasil, da guerra do paraguai e do impeachment do collor

---
+++ Assets/GameHB/Scripts/Utils/OscilacaoDosBarcos.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscilacaoDosBarcos : MonoBehaviour {
+
+    int angle;
+    [SerializeField]
+    float amplitude;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Movement());
+	}
+
+    IEnumerator Movement()
+    {
+        while (true)
+        {
+            angle++;
+            transform.position += Vector3.up * Mathf.Sin(angle/2) * amplitude;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
